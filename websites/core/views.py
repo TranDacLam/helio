fa06bbd0da4d3b7ab0085bf2941d7f5acd0118c0
nhung <nhunghtc1996@gmail.com>
2017-03-08 17:10:38 +0700

    Front end for Game Detail

--- websites/core/views.py
+++ websites/core/views.py
@@ -56,3 +56,8 @@ def combo_product(request):
     print "***START HELIO PLAY PAGE***"
     
     return render(request, 'websites/combo_product.html')
+
+def game_detail(request):
+    print "***START EVENT CONTENT PAGE***"
+    
+    return render(request, 'websites/game_detail.html')
