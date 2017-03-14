6f7e6f0641cf41a860bc2f67e4b7e8fca6b73132
Diem Nguyen <you@example.com>
2017-03-14 08:41:32 +0700

    api version1

--- websites/api/urls.py
+++ websites/api/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls import url, include
-import views
+from api import views
 
 urlpatterns = [
-    
+    url(r'^hots/$', views.hots),
+    url(r'^game/filter/$', views.game_filter),
+    url(r'^games/$', views.games),
+    url(r'^game/(?P<game_id>[0-9]+)/detail/$', views.game_detail),
+    url(r'^faqs/$', views.faqs),
+    url(r'^entertainments/$', views.entertainments),
+    url(r'^entertainments/(?P<entertainment_id>[0-9]+)/detail/$', views.entertainment_detail),
+    url(r'^event/filter/$', views.event_filter),
+    url(r'^events/$', views.events),
+    url(r'^posts/$', views.posts),
+
 ]
--- websites/api/views.py
+++ websites/api/views.py
@@ -1,2 +1,162 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from core.models import *
+from api.serializers import *
+from rest_framework.decorators import api_view
+from rest_framework import status
 
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+"""
+    Get Hots List to show in Homepage
+"""
+@api_view(['GET'])
+def hots(request):
+    try:
+        hot_list = Hots.objects.filter(is_show=True).order_by('date_created')[:5]
+        serializer = HotsSerializer(hot_list, many=True)
+        return JSONResponse(serializer.data)
+    except Exception ,e :
+        error = {"code" : 500, "message": "%s" % e, "fields": ""}
+        return JSONResponse( error, status=500)
+
+"""
+    Get All Game Filter
+"""
+@api_view(['GET'])
+def game_filter(request):
+    try:
+        game_filter_list = Game_Filter.objects.all()
+        serializer = GameFilterSerializer(game_filter_list, many=True)
+        return JSONResponse(serializer.data)
+    except Exception ,e :
+        error = {"code" : 500, "message": "%s" % e, "fields": ""}
+        return JSONResponse( error, status=500)
+    
+"""
+    Get All Game Play by Filter
+"""
+@api_view(['GET'])
+def games(request):
+    try:
+        game_filter =  request.GET.get("game_filter")
+        category_id =  request.GET.get("category_id")
+        print game_filter, category_id
+        game_list = Game.objects.filter(category_id=category_id)
+        serializer = GameSerializer(game_list, many=True)
+        return JSONResponse(serializer.data)
+    except Exception ,e :
+        error = {"code" : 500, "message": "%s" % e, "fields": ""}
+        return JSONResponse( error, status=500)
+
+"""
+    Get Game Detail by ID
+"""
+@api_view(['GET'])
+def game_detail(request, game_id):
+    try:
+        game_detail = Game.objects.get(pk=game_id)
+        serializer = GameDetailSerializer(game_detail, many=False)
+        return JSONResponse(serializer.data)
+    except Game.DoesNotExist, e:
+        error = {"code" : 400, "message": "%s" % e, "fields": "id"}
+        return JSONResponse( error, status=400)
+    except Exception ,e :
+        error = {"code" : 500, "message": "%s" % e, "fields": ""}
+        return JSONResponse( error, status=500)
+  
+"""
+    Get ALL FAQs
+"""  
+@api_view(['GET'])
+def faqs(request):
+    try:
+        faq_list = FAQs.objects.all()
+        serializer = FAQsSerializer(faq_list, many=True)
+        return JSONResponse(serializer.data)
+    except Exception ,e :
+        error = {"code" : 500, "message": "%s" % e, "fields": ""}
+        return JSONResponse( error, status=500)
+ 
+"""
+    Get Entertainments ( Entertainments containt promotion, night life stores, market kiosks. )
+""" 
+@api_view(['GET'])
+def entertainments(request):
+    try: 
+        entertainments_filter =  request.GET.get("entertainments_filter")
+        category_id =  request.GET.get("category_id")
+        entertainment_list = Entertainments.objects.all()
+        serializer = EntertainmentsSerializer(entertainment_list, many=True)
+        return JSONResponse(serializer.data)
+    except Exception ,e :
+        error = {"code" : 500, "message": "%s" % e, "fields": ""}
+        return JSONResponse( error, status=500)
+
+"""
+    Get Entertainment Detail By ID
+""" 
+@api_view(['GET'])
+def entertainment_detail(request, entertainment_id):
+    try:
+        entertainment_detail = Entertainments.objects.get(pk=entertainment_id)
+        serializer = EntertainmentDetailSerializer(entertainment_detail, many=False)
+        return JSONResponse(serializer.data)
+    except Game.DoesNotExist, e:
+        error = {"code" : 400, "message": "%s" % e, "fields": "id"}
+        return JSONResponse( error, status=400)
+    except Exception ,e :
+        error = {"code" : 500, "message": "%s" % e, "fields": ""}
+        return JSONResponse( error, status=500)
+
+"""
+    Get All Event Filter
+""" 
+@api_view(['GET'])
+def event_filter(request):
+    try:
+        event_filter_list = Event_Filter.objects.all()
+        serializer = EventFilterSerializer(event_filter_list, many=True)
+        return JSONResponse(serializer.data)
+    except Exception ,e :
+        error = {"code" : 500, "message": "%s" % e, "fields": ""}
+        return JSONResponse( error, status=500)
+
+
+"""
+    Get Events by Filter
+""" 
+@api_view(['GET'])
+def events(request):
+    try:
+        event_filter = request.GET.get("event_filter")
+        event_list = Event_Filter.objects.all()
+        serializer = EventsSerializer(event_list, many=True)
+        return JSONResponse(serializer.data)
+    except Exception ,e :
+        error = {"code" : 500, "message": "%s" % e, "fields": ""}
+        return JSONResponse( error, status=500)
+
+
+"""
+    Get posts details
+""" 
+@api_view(['GET'])
+def posts(request):
+    try:
+        id_or_key_query = request.GET.get("id_or_key_query")
+        print id_or_key_query
+        post_list = Posts.objects.all()
+        serializer = PostsSerializer(post_list, many=True)
+        return JSONResponse(serializer.data)
+    except Exception ,e :
+        error = {"code" : 500, "message": "%s" % e, "fields": ""}
+        return JSONResponse( error, status=500)
+
+
+ 
