dd0715d20ac0859227a24e871d32f352718aa873
Diem Nguyen <you@example.com>
2017-04-03 08:28:19 +0700

    add sql server config

--- websites/config/setting_local.py
+++ websites/config/setting_local.py
@@ -8,5 +8,13 @@ DATABASES = {
         'PORT': 3306,
         'USER': 'root',
         'PASSWORD': 'root'
+    },
+    'sql_db': {
+        'NAME': 'ECS7',
+        'ENGINE': 'sqlserver_pymssql',
+        'HOST': '192.168.1.212\SQLEXPRESS',
+        'USER': 'sa',
+        'PASSWORD': 'abcde12345-',
+        'POST':1433
     }
 }
