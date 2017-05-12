f6d1151882bb90770f79c3a22dc21c94031dae05
Diem Nguyen <you@example.com>
2017-05-12 09:40:01 +0700

    news + like button

--- websites/core/urls.py
+++ websites/core/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     url(r'^night-life/$', views.night_life, name='night_life'),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^news/$', views.news, name='news'),
+    url(r'^news/(?P<new_id>[0-9]+)/detail/$', views.new_detail, name='new_detail'),
     url(r'^careers/$', views.careers, name='careers'),
     url(r'^careers/(?P<career_id>[0-9]+)/detail/$', views.career_detail, name='career_detail'),
     url(r'^experience/$', views.experience, name='experience'),
