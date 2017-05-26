845aec1a37b7f8301a7c27506d0a71b167154ca9
tiendang <tiendang@tiendangs-iMac.local>
2017-05-26 14:39:58 +0700

    debug 1

--- websites/settings.py
+++ websites/settings.py
@@ -195,7 +195,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
-    'UNICODE_JSON': False,
+    'UNICODE_JSON': True,
     'EXCEPTION_HANDLER': 'api.views.custom_exception_handler'
 }
 JWT_AUTH = {
