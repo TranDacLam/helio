d305793a3ba19398fb040e27560c61fc2d0b5647
Diem Nguyen <you@example.com>
2017-05-26 13:14:07 +0700

    unicode api

--- websites/settings.py
+++ websites/settings.py
@@ -184,6 +184,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
