53d2aa8618f52c7f2c32f565fff7a56b2ffab33d
tiendang <tiendang@tiendangs-iMac.local>
2017-06-14 13:38:04 +0700

    add field personal_id

--- websites/api/serializers.py
+++ websites/api/serializers.py
@@ -28,7 +28,7 @@ class UserSerializer(SetCustomErrorMessagesMixin, serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'email', 'first_name', 'last_name', 'birth_date', 'phone', 'personal_id', 'country', 'address', 'city', 'device_uid', 'avatar',)
+        fields = ('username', 'password', 'email', 'first_name', 'last_name', 'birth_date', 'phone', 'personal_id', 'country', 'address', 'city', 'device_uid', 'avatar', 'is_staff')
         custom_error_messages_for_validators = {
             'email': {
                 UniqueValidator: _('This email is already taken. Please, try again')
