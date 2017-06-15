b067585f75503e5a2200c50fe4465175a8fe16a9
tiendang <tiendang@tiendangs-iMac.local>
2017-06-15 16:59:30 +0700

    add FCM API KEY

--- websites/settings.py
+++ websites/settings.py
@@ -294,6 +294,7 @@ CODE_LEN = 7 # Default code length
 NOCAPTCHA = True
 
 PUSH_NOTIFICATIONS_SETTINGS = {
+    "FCM_API_KEY": "AAAAMkND2_U:APA91bEVkDFA8uACGPTTj-Vc86kg4fuyhrPuUmGHJdzkuBaaJh4ZQuc09zMZCEt2xaSj5Xi7opPT9OZHq-hxDrWmqfkRGqRv38uC2nqHHK3Xwy-jwglWoSwIYywpT-qcsoW9TKAsiUayeRAkj_AYJ0AG-D02Ubx0jg",
     "APNS_CERTIFICATE": os.path.join(BASE_DIR, "key_apns/pem_dev/push_dev.pem"),
     "APNS_USE_SANDBOX": True,
     "APNS_TOPIC": "vn.vooc.helio.mobile",
