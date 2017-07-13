536a68f6c977a61ea5f727d70b5cbe4ef181f8d8
tiendang <tiendang@tiendangs-iMac.local>
2017-07-13 16:11:03 +0700

    fix phone blank

--- websites/api/views.py
+++ websites/api/views.py
@@ -179,6 +179,9 @@ def user_info(request):
             user = request.user
             # verify phone number
             phone = request.data.get('phone', '')
+            if not phone:
+                return Response({'flag': False, 'message': _('This phone number is empty. Please check again.')})
+
             qs = User.objects.filter(phone=phone).exclude(pk=user.id)
             if qs.count() > 0:
                 return Response({'flag': False, 'message': _('This phone number has already. Please choice another.')})
