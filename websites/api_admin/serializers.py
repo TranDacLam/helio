544da243e5380deca309b82b18946afc2415f89d
huyhoang <huyhoang1996ha@gmail.com>
2018-01-25 15:17:51 +0700

    search and update user app

--- websites/api_admin/serializers.py
+++ websites/api_admin/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from core.models import *
+from core.custom_models import *
 from django.contrib.auth import get_user_model
 
 
@@ -11,3 +12,22 @@ class HotsSerializer(serializers.Serializer):
     sub_url = serializers.CharField(max_length=1000)
     image = serializers.ImageField(max_length=1000)
     is_show = serializers.BooleanField()
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = User
+		fields=('full_name', 'email', 'phone','birth_date', 'personal_id', 'address')
+
+	def update(self, instance, validated_data):
+		instance.full_name = validated_data.get('full_name', instance.full_name)
+		instance.email = validated_data.get('email', instance.email)
+		instance.phone = validated_data.get('phone', instance.phone)
+		instance.birth_date = validated_data.get('birth_date', instance.birth_date)
+		instance.personal_id = validated_data.get('personal_id', instance.personal_id)
+		instance.address = validated_data.get('address', instance.address)
+		instance.save()
+		return instance
+
+
