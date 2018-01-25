645b949acfb48efe426af513892c8a06be5bd16d
diemnguyen <diemnguyen@vooc.vn>
2018-01-25 10:27:36 +0700

    add class customer

--- admin-marketing/src/app/shared/class/customer.ts
+++ admin-marketing/src/app/shared/class/customer.ts
@@ -1,11 +1,10 @@
-export class User {
-    id: number;
-    username: string;
+export class Customer {
+	customer_id: number;
+    barcode: string;
     full_name: string;
     birth_date: string;
     email: string;
     phone: string;
-    barcode: string;
     personal_id: string;
     address: string;
 }
