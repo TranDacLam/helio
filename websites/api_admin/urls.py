45df3dd082d2ad6bd2bbb007b12cf07354d141e5
huyhoang <huyhoang1996ha@gmail.com>
2018-01-26 09:02:56 +0700

    set url for user

--- websites/api_admin/urls.py
+++ websites/api_admin/urls.py
@@ -5,7 +5,7 @@ from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
 urlpatterns = [
 	url(r'^hots_admin/$', views.hots, name="get-hosts-admin"),
-	url(r'^user/$', views.UserDetail.as_view(), name="get-hosts-admin"),
-	url(r'^user/(?P<id>[0-9]+)/$', views.UserDetail.as_view(), name="get-hosts-admin"),
+	url(r'^user/$', views.UserDetail.as_view(), name="user"),
+	url(r'^user/(?P<id>[0-9]+)/$', views.UserDetail.as_view(), name="user-detail"),
 
 ]
