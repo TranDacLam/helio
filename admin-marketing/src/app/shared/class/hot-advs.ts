f4096c9cfcbcc625152ab01a6b45b8b9e978b06b
trangle <trangle@vooc.vn>
2018-02-05 17:34:44 +0700

    Front-end hot-advs

---
+++ admin-marketing/src/app/shared/class/hot-advs.ts
@@ -0,0 +1,11 @@
+export class HotAdvs {
+	id: number;
+	name: string;
+	image: string|any;
+	content: string;
+	check_res: boolean;
+	check_detail: boolean;
+	sub_url_res: string;
+	sub_url_detail: string;
+	is_draft: boolean;
+}
