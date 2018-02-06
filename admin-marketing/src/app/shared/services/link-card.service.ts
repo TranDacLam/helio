5b1079584d9d3bd4a30721458da83382c1b9ffed
trangle <trangle@vooc.vn>
2018-02-06 17:45:33 +0700

    Update enviroment url service

--- admin-marketing/src/app/shared/services/link-card.service.ts
+++ admin-marketing/src/app/shared/services/link-card.service.ts
@@ -5,6 +5,7 @@ import { Customer } from '../class/customer';
 import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import "rxjs/add/operator/catch";
+import { api } from '../utils/api';
 
 const httpOptions = {
     headers: new Headers({ 'Content-Type': 'application/json' })
@@ -94,6 +95,21 @@ export class LinkCardService {
         return this.http.post(this.url_relate, obj_relate, httpOptions).map((res: Response) => res.json()).catch(this.handleError);
     }
 
+    /* function getAllLinkedUsers(): Get all linked card users
+        author: Trangle
+    */
+    getAllLinkedUsers(): Observable<User[]> {
+       const url = `${api.user_link_card}`;
+       return this.http.get(url).map((res: Response) => res.json()).catch(this.handleError);
+    }
+    /* Delete All checkbox chosen
+        author: TrangLe
+    */
+
+    deleteAllUserLinkedSelected(user_linked_id: User[]): Observable<User[]> {
+        const url = `${api.user_link_card}?user_linked_id=${user_linked_id}`;
+        return this.http.delete(url, httpOptions).map((res: Response) => res.json()).catch(this.handleError);
+    }
 
     // exception
     private handleError(error: Response) {
--- admin-marketing/src/app/shared/utils/api.ts
+++ admin-marketing/src/app/shared/utils/api.ts
@@ -3,5 +3,24 @@ import { env } from '../../../environments/environment';
 export const api = {
   	promotion_list: env.api_domain + 'promotion_list/', 
   	promotion_detail: env.api_domain + 'promotion/',
-  	user_promotion: env.api_domain + 'user_promotion/'
+  	user_promotion: env.api_domain + 'user_promotion/',
+
+  	user_link_card: env.api_domain + 'user_link_card/',
+
+  	feedback: env.api_domain + 'feedback/',
+
+  	advertisement: env.api_domain + 'advertisement/',
+
+  	denomination: env.api_domain + 'denomination/',
+
+  	hot_adv: env.api_domain + 'hot_adv/',
+
+  	promotion_label: env.api_domain + 'promotion_label/',
+
+  	promotion_type: env.api_domain + 'promotion-type/',
+
+
+
+
+
 };
