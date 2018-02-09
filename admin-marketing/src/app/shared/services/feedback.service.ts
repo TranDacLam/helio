15fc5d13dafd15ca84cf59e53db733b601e75bde
trangle <trangle@vooc.vn>
2018-02-09 17:01:42 +0700

    Update Feedback

--- admin-marketing/src/app/shared/services/feedback.service.ts
+++ admin-marketing/src/app/shared/services/feedback.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Http, Headers, Response } from "@angular/http";
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders,HttpParams } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
@@ -16,47 +16,59 @@ const httpOptions = {
 
 @Injectable()
 export class FeedbackService {
-
+	// filter = new Filter();
 	constructor(private http: HttpClient) { }
 
-	// Get All Feedack
+	/*
+		GET: Get All Feedback Server
+	 */
 	getAllFeedback(): Observable<Feedback[]> {
-    let urlFeedback = `${api.feedback}`;
+    	let urlFeedback = `${api.feedback}`;
 		return this.http.get<Feedback[]>(urlFeedback).catch(this.handleError)
 	}
-	// GET: get Feedback by Id
+	/*
+		GET: Get Feedback By ID From Server
+	 */
 	getFeedbackById(id: number): Observable<Feedback> {
 		const url = `${api.feedback}${id}/`;
 		return this.http.get<Feedback>(url, httpOptions).catch(this.handleError)
 	}
 
-  // GET: get Feedback by Status
-  getFeedbackFilter(filter: {status:string|null, rate:string|null, start_date:string|null, end_date: any|null}): Observable<Feedback[]> {
-    const url = `${api.feedback}?status=${filter.status}&rate=${filter.rate}&start_date=${filter.start_date}&end_date=${filter.end_date}`;
-    return this.http.get<Feedback[]>(url).catch(this.handleError)
-  }
-
-	// PUT: Edit Feedback by id
+	/*
+		GET: Get Feedback By Filter
+		Filter: Status, Rate, Start_date, End_date
+	 */
+  	getFeedbackFilter(filter: {status?:string, rate?:string, start_date?:string, end_date?:string}): Observable<Feedback[]> {
+  		return this.http.get<Feedback[]>(`${api.feedback}`, {params: filter})
+  	}
+  	/*
+  		PUT: Edit Feedback By ID
+  	 */
 	updateFeedbackById(feedback: Feedback): Observable<any> {
 		const id = feedback.id;
 		const url = `${api.feedback}${id}/`;
 		return this.http.put<Feedback>(url, feedback, httpOptions).catch(this.handleError)
 	}
-	// DELETE: Delete Feedback by id
+	/*
+		DELETE: Delete Feedback By ID
+	*/
 	deleteFeedbackById(feedback: Feedback): Observable<Feedback> {
 		const id = feedback.id;
 		const url = `${api.feedback}${id}/`;
 		return this.http.delete<Feedback>(url,httpOptions).catch(this.handleError)
 	}
-	// Delete All Feedback which checkbox
-	
+	/*
+		DELETE: Delete All Feedback chosen
+	 */
 	deleteAllFeedbackChecked(fed_id: Feedback[]): Observable<Feedback[]> {
-    const url = `${api.feedback}?fed_id=${fed_id}`;
-    return this.http.delete<Feedback[]>(url, httpOptions)
-    .catch(this.handleError)
-  }
+    	const url = `${api.feedback}?fed_id=${fed_id}`;
+    	return this.http.delete<Feedback[]>(url, httpOptions)
+    		.catch(this.handleError)
+  	}
 
-	// Handle error
+	/*
+		Handler Error
+	*/
 	handleError(error: Response) {
 		return Observable.throw(error);
 	}
