921a49ac40e6f11d00257bba28b23d2f8efa7dce
tranlam <tdlam2105@gmail.com>
2018-03-05 09:14:21 +0700

    Fix submit feedback statistic

--- admin-marketing/src/app/components/feedback/statistics-feedback/statistics-feedback.component.ts
+++ admin-marketing/src/app/components/feedback/statistics-feedback/statistics-feedback.component.ts
@@ -44,56 +44,43 @@ export class StatisticsFeedbackComponent implements OnInit {
     }
 
     /* 
-        function onSubmitStatus():
+        function onSubmit(event):
+         + Check event is status or rate
          + Check date start and end.
          + Valid success, call service function searchStatisticFeedback to get statistic feedback status
          + Fail, show messsage error
         author: Lam
     */
-    onSubmitStatus(){
-        let start = this.date_status_start ? this.date_status_start : null;
-        let end = this.date_status_end ? this.date_status_end : null;
+    onSubmit(event){
+        if(event === 'status'){
+            let start = this.date_status_start ? this.date_status_start : null;
+            let end = this.date_status_end ? this.date_status_end : null;
+        }else{
+            let start = this.date_rate_start ? this.date_rate_start : null;
+            let end = this.date_rate_end ? this.date_rate_end : null;
+        }
         if(start == null && end == null){
             this.message_status = '* Vui lòng chọn ngày.';
         }else if(start == null || end == null || start <= end){
-            let custom_start = start ? this.formatDate(start): null;
-            let custom_end = end ? this.formatDate(end): null;
-            this.feedbackService.searchStatisticFeedback('status', custom_start, custom_end).subscribe(
+            let custom_start = start ? this.formatDate(start): '';
+            let custom_end = end ? this.formatDate(end): '';
+            this.feedbackService.searchStatisticFeedback(event, custom_start, custom_end).subscribe(
                 (data) => {
-                    this.fb_status = data.message.status;
-                    this.fb_rate = data.message.rate;
+                    if(event === 'status'){
+                        this.fb_status = data.message.status;
+                        this.status_sum = data.message.status_sum;
+                    }else{
+                        this.fb_rate = data.message.rate;
+                        this.rate_sum = data.message.rate_sum;
+                    }
+                },
+                (error) => {
+                    this.router.navigate(['/error']);
                 }
             );
             this.message_status = '';
         }else{
             this.message_status = '* Vui lòng nhập ngày kết thúc lớn hơn hoặc bằng ngày bắt đầu';
-        }
-    }
-
-    /* 
-        function onSubmitStatus():
-         + Check date start and end.
-         + Valid success, call service function searchStatisticFeedback to get statistic feedback rate 
-         + Fail, show messsage error
-        author: Lam
-    */
-    onSubmitRate(){
-        let start = this.date_rate_start ? this.date_rate_start : null;
-        let end = this.date_rate_end ? this.date_rate_end : null;
-        if(start == null && end == null){
-            this.message_rate = '* Vui lòng chọn ngày.';
-        }else if(start == null || end == null || start <= end){
-            let custom_start = start ? this.formatDate(start): null;
-            let custom_end = end ? this.formatDate(end): null;
-            this.feedbackService.searchStatisticFeedback('rate', custom_start, custom_end).subscribe(
-                (data) => {
-                    this.status_sum = data.message.status_sum;
-                    this.rate_sum = data.message.rate_sum;
-                }
-            );
-            this.message_rate = '';
-        }else{
-            this.message_rate = '* Vui lòng nhập ngày kết thúc lớn hơn hoặc bằng ngày bắt đầu';
         }
     }
 
