d98866aae19e62e2645b12165ea45ee27743b5d9
diemnguyen <diemnguyen@vooc.vn>
2018-03-08 17:45:28 +0700

    tesst

--- admin-marketing/src/environments/environment.prod.ts
+++ admin-marketing/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const env = {
-  production: true
+  	production: true,
+  	api_domain: 'http://172.16.12.10:8000/vi/api/',
+  	api_domain_root: 'http://172.16.12.10:8000',
+    key_recaptcha: '6LcHzEoUAAAAAG1ux2034oAvIlJ7hFDIaCwrRzRG'
 };
