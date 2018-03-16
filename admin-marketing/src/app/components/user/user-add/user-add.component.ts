09ecfb5aaa51c27c9795b7f67d6d89c7690bf8ff
trangle <trangle@vooc.vn>
2018-03-16 17:27:29 +0700

    Fix bugs user

--- admin-marketing/src/app/components/user/user-add/user-add.component.ts
+++ admin-marketing/src/app/components/user/user-add/user-add.component.ts
@@ -8,6 +8,7 @@ import { Role } from '../../../shared/class/role';
 import { UserService } from '../../../shared/services/user.service';
 import { RoleService } from '../../../shared/services/role.service';
 import { UserValidators } from './../../../shared/validators/user-validators';
+import { ValidateSubmit } from './../../../shared/validators/validate-submit';
 
 import { DatePipe } from '@angular/common';
 import * as moment from 'moment';
@@ -50,19 +51,19 @@ export class UserAddComponent implements OnInit {
      */
  	createForm() {
  		this.formUser = this.fb.group({
-            email: [this.user_form.email, [Validators.required, Validators.email]],
-            full_name: [this.user_form.full_name, [Validators.required]],
+            email: [this.user_form.email, [Validators.required, UserValidators.emailValidators]],
+            full_name: [this.user_form.full_name],
             phone: [this.user_form.phone, [Validators.required,UserValidators.phoneValidators]],
             personal_id: [this.user_form.personal_id],
             country: [this.user_form.country],
             address: [this.user_form.address],
             city: [this.user_form.city],
-            avatar: [this.user_form.avatar, [Validators.required]],
+            avatar: [this.user_form.avatar],
             password: [this.user_form.password, [Validators.required, UserValidators.passwordValidators]],
             is_active: [this.user_form.is_active],
             is_staff: [this.user_form.is_staff],
             role: [this.user_form.role, [Validators.required]],
-            birth_date: [this.user_form.birth_date ? moment(this.user_form.birth_date,"DD/MM/YYYY").toDate() : null],
+            birth_date: [this.user_form.birth_date ? moment(this.user_form.birth_date,"DD/MM/YYYY").toDate() : null, [UserValidators.birtdateValidators]],
         });
  	}
 
@@ -88,21 +89,25 @@ export class UserAddComponent implements OnInit {
         @author: Trangle
      */
     onSubmit() {
-        var self = this;
-        let userFormGroup = this.convertFormGroupToFormData(this.formUser);
-        this.userService.createUser(userFormGroup).subscribe(
-            (data) => {
-                self.users.push(data);
-                self.router.navigate(['/user-list', { message_post: this.formUser.value['email']} ]);
-            },
-            (error) => {
-                if(error.code == 400) {
-                    self.errorMessage = error.message;
-                } else {
-                    this.router.navigate(['/error', { message: error.message }]);
+        if (this.formUser.invalid) {
+            ValidateSubmit.validateAllFormFields(this.formUser);
+        } else {
+            var self = this;
+            let userFormGroup = this.convertFormGroupToFormData(this.formUser);
+            this.userService.createUser(userFormGroup).subscribe(
+                (data) => {
+                    self.users.push(data);
+                    self.router.navigate(['/user-list', { message_post: this.formUser.value['email']} ]);
+                },
+                (error) => {
+                    if(error.code == 400) {
+                        self.errorMessage = error.message;
+                    } else {
+                        this.router.navigate(['/error', { message: error.message }]);
+                    }
                 }
-            }
-        )
+            )
+        }
     }
 
     /* 
