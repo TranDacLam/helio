edfc8a3d8319c6f7631eab623ccda4d00bf85a7f
trangle <trangle@vooc.vn>
2018-03-21 16:52:32 +0700

    Fix bugs checkbox all checked with data empty

--- admin-marketing/src/app/components/link-card/link-card-list/link-card-list.component.ts
+++ admin-marketing/src/app/components/link-card/link-card-list/link-card-list.component.ts
@@ -136,7 +136,11 @@ export class LinkCardListComponent implements OnInit {
 
     // input checkall checked/unchecked
     checkSelectAllCheckbox() {
-        $('#select-all').prop('checked', $("#table_id tr.row-data:not(.selected)").length == 0);
+        if($('#table_id tbody tr').hasClass('selected')){
+            $('#select-all').prop('checked', $("#table_id tr.row-data:not(.selected)").length == 0);
+        }else{
+            $('#select-all').prop('checked', false);
+        }
         this.getLengthSelected();
     }
     /*
