3d6e7adf3baa46c6cd4c46374d9b1c49a2784fe1
diemnguyen <diemnguyen@vooc.vn>
2018-04-20 18:43:03 +0700

    fxi bgus denomination

--- admin-marketing/src/app/shared/validators/denomination-validators.ts
+++ admin-marketing/src/app/shared/validators/denomination-validators.ts
@@ -3,25 +3,26 @@ import { FormControl, FormBuilder, FormArray, ValidationErrors } from '@angular/
 export class DenominationValidators {
 
 	static denominationValidators(c: FormControl): ValidationErrors {
-		let denomiRegx = /^[0-9,]+$/;
 		let denomiVal = c.value;
-		let denomiValues = denomiVal.match(denomiRegx);
-		
-		if (denomiVal !== '') {
-			if (denomiValues === null) {
-				return {
-                	'denominationValidate': {
-                    	'message': 'Vui lòng nhập mệnh giá tiền hợp lệ.'
-                	}
-            	};
-        	} else if (denomiVal == 0) {
-        		return {
-                	'denominationValidate': {
-                    	'message': 'Vui lòng nhập mệnh giá tiền lớn hơn 0.'
-                	}
-            	};
-        	}
-    	}
-    	return null;
+		let val_num = denomiVal ? denomiVal.replace(/,/g, '') : denomiVal;
+
+		if (!denomiVal) {
+			return;
+		} else
+		if (isNaN(val_num)) {
+			return {
+            	'denominationValidate': {
+                	'message': 'Vui lòng nhập mệnh giá tiền hợp lệ.'
+            	}
+        	};
+		} else if (Number(val_num) == 0) {
+			return {
+            	'denominationValidate': {
+                	'message': 'Vui lòng nhập mệnh giá tiền lớn hơn 0.'
+            	}
+        	};
+		}
+    	return null; 
+
 	}
 }
