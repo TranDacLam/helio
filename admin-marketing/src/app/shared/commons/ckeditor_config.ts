68e74502a632a61e3bbd2b0e1312769d35de305c
diemnguyen <diemnguyen@vooc.vn>
2018-04-23 16:19:18 +0700

    test + fixx ckeditỏ

--- admin-marketing/src/app/shared/commons/ckeditor_config.ts
+++ admin-marketing/src/app/shared/commons/ckeditor_config.ts
@@ -2,5 +2,7 @@ import { env } from './../../../environments/environment';
 
 export let config = {
     language: 'vi',
-    filebrowserUploadUrl: env.api_domain_root+'/admin/ckeditor/upload/'
+    filebrowserUploadUrl: env.api_domain_root+'/admin/ckeditor/upload/',
+    extraPlugins:'youtube',
+    tabSpaces:4
 }
