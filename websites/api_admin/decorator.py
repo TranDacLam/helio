b24be2650803ba323a4b20bfb943a3a133649358
huyhoang <huyhoang1996ha@gmail.com>
2018-05-07 14:53:25 +0700

    add decorator and model_key.py

---
+++ websites/api_admin/decorator.py
@@ -0,0 +1,41 @@
+from core.models import Roles_Permission
+from rest_framework.response import Response
+from django.utils.translation import ugettext_lazy as _  
+
+def check_role_permission(model_key):
+    def wrapper(view_func):
+        def wrapped(self, request, *args, **kwargs):
+            print "Check role permission"
+            # Check user is login
+            if not request.user.is_authenticated():
+                return Response({"code": 401, "message": _("Unauthorized"), "fields": ""}, status=401)
+            try:
+                # Check user have role. If user not set role then can not access
+                role = request.user.role
+                if not role:
+                    return Response({"code": 403, "message": _("User has no role."), "fields": ""}, status=403)
+                
+                # Get promision by role + model key
+                role_permission = Roles_Permission.objects.filter(
+                    role=role, model_name__key=model_key).get()
+                
+                # If permission is change then access method 'GET, POST, PUT'
+                if role_permission.permission == 'change' and request.method == 'DELETE':
+                    return Response({"code": 403, "message": _("Forbidden"), "fields": ""}, status=403)
+
+                # If permission is read then access only method GET
+                if role_permission.permission == 'read' and request.method != 'GET':
+                    return Response({"code": 403, "message": _("Forbidden"), "fields": ""}, status=403)
+
+                return view_func(self, request, *args, **kwargs)
+
+            except Roles_Permission.DoesNotExist, e:
+                print "Roles_Permission with model: %s, role: %s not found" % (model_key, role)
+                return Response({"code": 403, "message": _("User don't have permission."), "fields": ""}, status=403)
+            except Exception, e:
+                print "Error: ", e
+                raise Exception(
+                    "ERROR : Internal Server Error .Please contact administrator.")
+
+        return wrapped
+    return wrapper
