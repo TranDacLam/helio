ae723688f5725e55a3cad4381330156f6efb1f82
tranlam <tdlam2105@gmail.com>
2018-05-08 10:52:48 +0700

    Change script ckeditor into main.ts

--- admin-marketing/src/main.ts
+++ admin-marketing/src/main.ts
@@ -1,11 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { env } from './environments/environment';
 
+document.write(
+    `
+        <script type="text/javascript" src="${env.api_domain_root}/static/assets/js/ckeditor/ckeditor.js">
+        </script>
+    `
+);
+
 if (env.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
