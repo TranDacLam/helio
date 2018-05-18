a3ea7e10d15737903d793e7149b3d869ed210aff
trangle <trangle@vooc.vn>
2018-05-18 14:30:12 +0700

    Fix bugs feedback validate message is required

--- websites/core/forms.py
+++ websites/core/forms.py
@@ -17,7 +17,7 @@ class ContactForm(forms.Form):
     email = forms.CharField(widget=forms.TextInput())
     phone = forms.CharField(widget=forms.TextInput())
     subject = forms.CharField(widget=forms.TextInput())
-    message = forms.CharField(widget=forms.TextInput(), required=False)
+    message = forms.CharField(widget=forms.TextInput())
     captcha = ReCaptchaField(required=True)
 
     def __init__(self, *args, **kwargs):
